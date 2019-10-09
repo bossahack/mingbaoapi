18b73b622ad195201e6dc8a12deb1e6541ec8ba8
licun <licun@benlai.com>
2019-10-09 10:27:03 +0800

    tes

---
+++ Book.Api/Filters/GlobalErrorFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Book.Api.Filters
+{
+    public class GlobalErrorFilterAttribute: ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            base.OnException(actionExecutedContext);
+        }
+    }
+}
--- Book.Api/obj/Release/AspnetCompileMerge/Source/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Book.Api.WebApiApplication" Language="C#" %>
