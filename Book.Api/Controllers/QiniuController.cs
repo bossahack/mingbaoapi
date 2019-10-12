86e1365d60c17778ab94a6b60f2a76ba637858ec
licun <licun@benlai.com>
2019-10-12 18:09:14 +0800

    qiniu

---
+++ Book.Api/Controllers/QiniuController.cs
@@ -0,0 +1,20 @@
+﻿using Book.Api.Filters;
+using Book.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Book.Api.Controllers
+{
+    public class QiniuController : ApiController
+    {
+        [ShopFilter]
+        public string GetSimpleKeyShop()
+        {
+            return QiniuService.GetInstance().GetSimpleKey();
+        }
+    }
+}
---
+++ Book.Service/QiniuService.cs
@@ -0,0 +1,35 @@
+﻿using Qiniu.Storage;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace Book.Service
+{
+    public class QiniuService
+    {
+        private static string AccessKey = WebConfigurationManager.AppSettings["AccessKey"];
+        private static string SecretKey = WebConfigurationManager.AppSettings["SecretKey"];
+        private static string Bucket = WebConfigurationManager.AppSettings["Bucket"];
+        private static QiniuService _Instance;
+        public static QiniuService GetInstance()
+        {
+            if (_Instance != null)
+                return _Instance;
+            _Instance = new QiniuService();
+            return _Instance;
+        }
+
+        public string GetSimpleKey()
+        {
+            Mac mac = new Mac(AccessKey, SecretKey);
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = Bucket;
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+            return token;
+        }
+    }
+}
