62f8604c5ea11afc8f20a58da9b95b9b371c82e8
licun <licun@benlai.com>
2019-12-09 17:56:17 +0800

    tt

---
+++ Book.Job/CloseUnPayShopJob.cs
@@ -0,0 +1,33 @@
+﻿using Book.Dal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.Job
+{
+    public class CloseUnPayShopJob
+    {
+        public void Execute(int retryCount = 0)
+        {
+            try
+            {
+                if (retryCount >= 3)
+                {
+                    return;
+                }
+
+                ShopDal.GetInstance().CloseUnPayShop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Trace.WriteLine(ex.ToString());
+                retryCount++;
+                Execute(retryCount);
+            }
+        }
+    }
+}
