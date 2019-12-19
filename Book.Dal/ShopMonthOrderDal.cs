a1e4971f660ce3cede257e9f0984784361fe3999
holly fred <1241205407@11.com>
2019-12-19 21:40:58 +0800

    job

--- Book.Dal/ShopMonthOrderDal.cs
+++ Book.Dal/ShopMonthOrderDal.cs
@@ -24,8 +24,8 @@ namespace Book.Dal
         {
             using (var conn = SqlHelper.GetInstance())
             {
-                conn.Execute(@"INSERT into shop_month_order(shop_id,year,month,qty,effect_qty,should_pay,status,user_fee_status,trade_no)
-SELECT id,@year,@month,0,0,0,0,0,CONCAT(year, RIGHT(CONCAT('0',`month`),2), RIGHT(CONCAT('000000',`shop_id`),7),'m') from shop where create_date>=@begin and create_date<@end and not EXISTS(select 1 FROM shop_month_order mo where mo.shop_id=shop.id and `month`=@month and year=@year)", new { begin = begin.ToString("yyyy-MM-dd"),end=end.ToString("yyyy-MM-dd"),year,month });
+                conn.Execute($@"INSERT into shop_month_order(shop_id,year,month,qty,effect_qty,should_pay,status,user_fee_status,trade_no)
+SELECT id,@year,@month,0,0,0,0,0,CONCAT(@year, {month.ToString().PadLeft(2,'0')}, RIGHT(CONCAT('000000',id),7),'m') from shop where DAY(create_date)>=DAY(@begin) and DAY(create_date)<DAY(@end) and not EXISTS(select 1 FROM shop_month_order mo where mo.shop_id=shop.id and `month`=@month and year=@year)", new { begin = begin.ToString("yyyy-MM-dd"),end=end.ToString("yyyy-MM-dd"),year,month });
             }
         }
 
@@ -36,7 +36,7 @@ SELECT id,@year,@month,0,0,0,0,0,CONCAT(year, RIGHT(CONCAT('0',`month`),2), RIGH
                 conn.Execute(@"
 UPDATE shop_month_order monthorder,(SELECT SUM( dayorder.effect_qty) effectqty,dayorder.shop_id from shop_day_order dayorder where  dayorder.date>=@orderBegin and dayorder.date<=@orderEnd GROUP BY dayorder.shop_id ) b,(SELECT SUM( dayorder.qty) qty,dayorder.shop_id from shop_day_order dayorder where  dayorder.date>=@orderBegin and dayorder.date<=@orderEnd GROUP BY dayorder.shop_id ) a
 set monthorder.effect_qty=b.effectqty,monthorder.qty=a.qty,should_pay=b.effectqty/10,status=@status,generate_date=now()
-WHERE year=@year and `month`=@month and monthorder.shop_id in (SELECT id FROM shop where create_date >=@createBegin and create_date<@createEnd) and monthorder.shop_id=b.shop_id and monthorder.shop_id=a.shop_id", new { createBegin = createBegin.ToString("yyyy-MM-dd"), createEnd = createEnd.ToString("yyyy-MM-dd"), orderBegin=orderBegin.ToString("yyyy-MM-dd"), orderEnd= orderEnd.ToString("yyyy-MM-dd"), year, month,status=(int)BillStatus.UnPay });
+WHERE year=@year and `month`=@month and monthorder.shop_id in (SELECT id FROM shop where Day(create_date) >=Day(@createBegin) and Day(create_date)<Day(@createEnd)) and monthorder.shop_id=b.shop_id and monthorder.shop_id=a.shop_id", new { createBegin = createBegin.ToString("yyyy-MM-dd"), createEnd = createEnd.ToString("yyyy-MM-dd"), orderBegin=orderBegin.ToString("yyyy-MM-dd"), orderEnd= orderEnd.ToString("yyyy-MM-dd"), year, month,status=(int)BillStatus.UnPay });
             }
 
         }
